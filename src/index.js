6932974daf1ea7da9184a07dafc8c624c52bc61c
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2023-03-14 16:37:36 -0300

    feat: requisito-01

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
+const readJsonData = require('./utils/fs/readJsonData');
 
+const talkerPath = path.resolve(__dirname, './talker.json');
 const app = express();
 app.use(express.json());
 
@@ -11,6 +15,11 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+app.get('/talker', async (req, res) =>{
+    const fileContent = await readJsonData(talkerPath);
+    return res.status(HTTP_OK_STATUS).json(fileContent);
+});
+
 app.listen(PORT, () => {
-  console.log('Online');
+  console.log('Ola, tudo bem?');
 });
---
+++ src/utils/fs/readJsonData.js
@@ -0,0 +1,17 @@
+const fs = require('fs').promises;
+
+const readJsonData = async(path) => {
+   const fileContent = await fs.readFile(path, 'utf-8');
+   const talkers = JSON.parse(fileContent);
+  //  console.log(talkers);
+   return talkers;
+};
+
+
+function main () {
+  readJsonData('./src/talker.json');
+};
+
+main();
+
+module.exports = readJsonData;
